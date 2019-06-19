09ad5284c755724643f7aed0a46c1d4ca4028a2f
Felix Meyer <phelixm@gmail.com>
2019-06-18 19:55:59 -0500

    simple changes for deployment

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,11 @@
 const app = require("../src/app");
 
 describe("App", () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it("GET / responds with 200 containing JSON with OK status", () => {
+    // this doesn't work. Look up what should be passed to .expect to fix this.
     return supertest(app)
-      .get("/")
-      .expect(200, "Hello, world!");
+      .get("/api/*")
+      .expect(res)
+      .to.deep.include({ ok: true });
   });
 });
