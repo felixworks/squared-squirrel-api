2049cd34c3accc48188a1e5b39da4b3b4286e45f
Felix Meyer <phelixm@gmail.com>
2019-06-29 13:02:15 -0500

    make CORS work for both localhost and deployed client

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
+const clientValue = () => {
+  if (process.env.NODE_ENV === production) {
+    return "https://squared-squirrel-app.felixworks.now.sh";
+  } else return "http://localhost:3000";
+};
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "localhost:3000",
+  CLIENT_ORIGIN: clientValue,
   DB_URL:
     process.env.DATABASE_URL ||
     "postgresql://squirrel@localhost/squared_squirrel"
