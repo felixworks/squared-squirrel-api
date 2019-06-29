c268523edb265164df5d7378fc89a9dd1ab3bdff
Felix Meyer <phelixm@gmail.com>
2019-06-29 13:04:51 -0500

    tweak CORS config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
-const clientValue = () => {
-  if (process.env.NODE_ENV === production) {
-    return "https://squared-squirrel-app.felixworks.now.sh";
-  } else return "http://localhost:3000";
-};
-
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: clientValue,
+  CLIENT_ORIGIN: "https://squared-squirrel-app.felixworks.now.sh",
   DB_URL:
     process.env.DATABASE_URL ||
     "postgresql://squirrel@localhost/squared_squirrel"
