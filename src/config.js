97129f43529a7653d47fd34efc8007b3f9c1a105
Felix Meyer <phelixm@gmail.com>
2019-06-29 00:31:10 -0500

    modify config for deployment

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: "localhost:3000",
   DB_URL:
-    process.env.DB_URL || "postgresql://squirrel@localhost/squared_squirrel"
+    process.env.DATABASE_URL ||
+    "postgresql://squirrel@localhost/squared_squirrel"
 };
