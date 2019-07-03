883d0044371a6fc3e78af46b3c5a26004c75dede
Felix Meyer <phelixm@gmail.com>
2019-07-02 20:54:38 -0500

    CORS change

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: "https://squared-squirrel-app.felixworks.now.sh",
-  //  "http://localhost:3000"
+  CLIENT_ORIGIN:
+    process.env.CLIENT_ORIGIN ||
+    "https://squared-squirrel-app.felixworks.now.sh",
   DB_URL:
     process.env.DATABASE_URL ||
     "postgresql://squirrel@localhost/squared_squirrel"
